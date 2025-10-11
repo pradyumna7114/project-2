5e9c97b198744da787ccd9709265447e0cd3f0c4
Pradhumna Mishra <your-github-email@example.com>
2025-10-11 17:10:33 +0530

    new

--- 100dayscodechallenge/js/d1.js
+++
@@ -1,7 +0,0 @@
-let nums=[1,2,3,4];
-const square = nums.map((num)=> num*num);
-console.log(square); 
-const sum= nums.reduce((acc,cur)=> acc+cur,0);//after comma 0 is the value 
-console.log(sum);                        // at which accumulator starts
-const avg =  sum/nums.length;
-console.log(avg);                               
